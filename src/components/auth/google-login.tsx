c582d7c476327859e7cd586ff2e0eb4a959ec318
glugnoob <r.gluger23@gmail.com>
2025-05-28 21:14:26 +0100

    added google oAuth

---
+++ src/components/auth/google-login.tsx
@@ -0,0 +1,38 @@
+import { authApi } from "@/lib/api";
+import { useAuthStore } from "@/stores/auth-store";
+import {
+  GoogleLogin,
+  GoogleOAuthProvider,
+  type CredentialResponse,
+} from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+
+const GoogleLoginButton = () => {
+  const loginUser = useAuthStore((state) => state.loginAuth);
+  const navigate = useNavigate();
+
+  const handleSuccess = async (credential: CredentialResponse) => {
+    const idToken = credential.credential;
+
+    try {
+      const res = await authApi.post("login/google/", { id_token: idToken });
+
+      const token = res.data.access;
+      loginUser(token);
+      navigate("/signed-in");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <GoogleLogin
+        onSuccess={handleSuccess}
+        onError={() => alert("Login failed")}
+      />
+    </GoogleOAuthProvider>
+  );
+};
+
+export default GoogleLoginButton;
--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,9 +1,12 @@
+import GoogleLoginButton from "@/components/auth/google-login";
 import UserForm from "@/components/forms/user-form";
 
 const LoginPage = () => {
   return (
-    <section className="p-20 text-xl">
+    <section className="p-20 text-xl flex flex-col gap-10">
       <UserForm />
+
+      <GoogleLoginButton />
     </section>
   );
 };
